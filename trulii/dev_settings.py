d7aef2c2ceefe3e5812fb711eaad308a4b83b621
Levi <levinoelvm@gmail.com>
2015-03-18 02:00:29 -0500

    updated procfile

--- trulii/dev_settings.py
+++ trulii/dev_settings.py
@@ -1,6 +1,17 @@
 ################AMAZON S3 STORAGE ##########################
 
 
+AWS_STORAGE_BUCKET_NAME = 'trulii-dev'
+AWS_ACCESS_KEY_ID = 'AKIAJRUNNQDO2LM6OSEA'
+AWS_SECRET_ACCESS_KEY = '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'
+
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
 
 
 ###STATIC#####
