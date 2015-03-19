efcc237817382a9402746da8b046d2d7ab740972
Levi <levinoelvm@gmail.com>
2015-03-19 22:06:15 +0000

    fixinheroku

--- trulii/local_settings.py
+++ trulii/local_settings.py
@@ -1,4 +1,4 @@
-
+import dj_database_url
 import os
 PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
@@ -11,16 +11,19 @@ STATICFILES_DIRS = (
 
 
 DATABASES = {}
-DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'trulii',  # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': 'trulii',
-        'PASSWORD': 'trulii',
-        'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',  # Set to empty string for default.
-    }
-}
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+# DATABASES = {
+#     'default': {
+#         # 'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'trulii',  # Or path to database file if using sqlite3.
+#         # The following settings are not used with sqlite3:
+#         'USER': 'trulii',
+#         'PASSWORD': 'trulii',
+#         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         'PORT': '',  # Set to empty string for default.
+#     }
+# }
     
