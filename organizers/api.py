d6600d392ac11984c250c7559bc7235d6f0d0407
Levi <levinoelvm@gmail.com>
2015-03-20 01:29:34 +0000

    sdasd

--- organizers/api.py
+++ organizers/api.py
@@ -1,37 +1,37 @@
-from .models import Organizer
-from .models import Instructor
+# from .models import Organizer
+# from .models import Instructor
 
-from .serializer import OrganizersSerializer
-from .serializer import InstructorsSerializer
+# from .serializer import OrganizersSerializer
+# from .serializer import InstructorsSerializer
 
-from django.http import Http404
+# from django.http import Http404
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
-
-class OrganizerDetail(APIView):
-	def get_object(self, pk):
-		try:
-			return Organizer.object.get(pk = pk)
-		except Organizer.DoesNotExist:
-			raise Http404
-
-	def get(self, request, pk, format = None):
-		organizer = self.get_object(pk)
-		serilized_organizers = OrganizersSerializer(organizer)
-		return Response(serilized_organizers.data)
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
 
 
-class InstructorDetail(APIView):
-	def get_object(self, pk):
-		try:
-			return Instructor.object.get(pk = pk)
-		except Instructor.DoesNotExist:
-			raise Http404
+# class OrganizerDetail(APIView):
+# 	def get_object(self, pk):
+# 		try:
+# 			return Organizer.object.get(pk = pk)
+# 		except Organizer.DoesNotExist:
+# 			raise Http404
 
-	def get(self, request, pk, format = None):
-		instructor = self.get_object(pk)
-		serilized_instructors = (instructor)
-		return Response(serilized_instructors.data)
+# 	def get(self, request, pk, format = None):
+# 		organizer = self.get_object(pk)
+# 		serilized_organizers = OrganizersSerializer(organizer)
+# 		return Response(serilized_organizers.data)
+
+
+# class InstructorDetail(APIView):
+# 	def get_object(self, pk):
+# 		try:
+# 			return Instructor.object.get(pk = pk)
+# 		except Instructor.DoesNotExist:
+# 			raise Http404
+
+# 	def get(self, request, pk, format = None):
+# 		instructor = self.get_object(pk)
+# 		serilized_instructors = (instructor)
+# 		return Response(serilized_instructors.data)
 
