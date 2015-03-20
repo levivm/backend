4e68d21fd1fd832223e0c901b98dc39ec5753ca1
Levi <levinoelvm@gmail.com>
2015-03-20 02:13:02 +0000

    dsa

--- trulii/urls.py
+++ trulii/urls.py
@@ -11,7 +11,7 @@ from users.views import ResetPassword,ChangeEmailView,PasswordChange
 
 
 #if settings.DEBUG :
-if True :
+if settings.DEBUG:
     urlpatterns = patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     )
