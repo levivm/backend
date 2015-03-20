cd117fa57c1ec622ca8c4bacff97ceacd2039357
Levi <levinoelvm@gmail.com>
2015-03-20 01:49:17 +0000

    ssadasd

--- trulii/dev_settings.py
+++ trulii/dev_settings.py
@@ -3,17 +3,17 @@ import dj_database_url
 ################AMAZON S3 STORAGE ##########################
 
 
-# AWS_STORAGE_BUCKET_NAME = 'trulii-dev'
-# AWS_ACCESS_KEY_ID = 'AKIAJRUNNQDO2LM6OSEA'
-# AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'trulii-dev'
+AWS_ACCESS_KEY_ID = 'AKIAJRUNNQDO2LM6OSEA'
+AWS_SECRET_ACCESS_KEY = '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'
 
 
-# # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
-# # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
-# # This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
-# # We also use it in the next setting.
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_FILE_OVERWRITE = False
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_FILE_OVERWRITE = False
 
 
 ###STATIC#####
@@ -35,7 +35,6 @@ STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config()
-#DATABASES['default']['engine'] = 'django.db.backends.postgresql_psycopg2'
 
 
 
