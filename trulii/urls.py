6d993f1aaaaa471b26562e4b7df7aa70bf143762
Levi <levinoelvm@gmail.com>
2015-03-20 04:03:07 +0000

    ss

--- trulii/urls.py
+++ trulii/urls.py
@@ -55,9 +55,10 @@ urlpatterns = patterns('',
 )
 
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, 'show_indexes': True}),
     )
 
 
