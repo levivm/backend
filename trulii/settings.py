37022f47546f96a2d44bb5678c3db3cca08e7182
Levi <levinoelvm@gmail.com>
2015-03-20 08:06:02 +0000

    x

--- trulii/settings.py
+++ trulii/settings.py
@@ -222,7 +222,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'trulii/staticfiles/')
 #STATIC_ROOT = '/static/'
 #
 
