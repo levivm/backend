b8fc75aaea0117cbdbebd8bfe16b86b589c4583e
Harvey Forero <harvey@trulii.com>
2015-04-24 13:48:25 -0500

    TRUL-148 #in-review Permisos para el módulo de actividades

--- activities/urls.py
+++ activities/urls.py
@@ -1,31 +1,33 @@
 from django.conf.urls import *
 #from activities import views
 from .views import ActivitiesViewSet,ListCategories,CategoriesViewSet,\
-				  SubCategoriesViewSet,TagsViewSet,ChronogramsViewSet
+				   SubCategoriesViewSet,TagsViewSet,ChronogramsViewSet,\
+                   AcitivityPhotosViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework import routers
 
 
 
 urlpatterns = patterns('',
-	#url(r'^categories/?$',ListCategories.as_view()),
+	#url(r'^/categories/?$',ListCategories.as_view()),
 	url(r'^/?$',ActivitiesViewSet.as_view({'get':'list','post':'create'})),
-	url(r'^(?P<pk>\d+)/?$',ActivitiesViewSet.as_view({'get':'retrieve','put': 'partial_update'})),
-	url(r'^(?P<activity_pk>\d+)/calendars/?$',ChronogramsViewSet.as_view({'get':'list','post':'create'})),
-	url(r'^(?P<activity_pk>\d+)/calendars/(?P<calendar_pk>\d+)/?$',ChronogramsViewSet.as_view({\
+	url(r'^/(?P<pk>\d+)/?$',ActivitiesViewSet.as_view({'get':'retrieve','put': 'partial_update'})),
+	url(r'^/(?P<activity_pk>\d+)/calendars/?$',ChronogramsViewSet.as_view({'get':'list','post':'create'})),
+	url(r'^/(?P<activity_pk>\d+)/calendars/(?P<calendar_pk>\d+)/?$',ChronogramsViewSet.as_view({\
 		                            'put': 'update','get':'retrieve','delete':'destroy'})),
 
-	# url(r'^(?P<pk>\d+)/calendars/?$',ActivitiesViewSet.as_view({'post':'create_calendar',\
+	# url(r'^/(?P<pk>\d+)/calendars/?$',ActivitiesViewSet.as_view({'post':'create_calendar',\
 	# 	                            'put': 'update_calendar','get':'get_calendars','delete':'delete_calendar'})),
 	
-	url(r'^(?P<pk>\d+)/publish/?$',ActivitiesViewSet.as_view({'put':'publish'})),
-	url(r'^(?P<pk>\d+)/gallery/?$',ActivitiesViewSet.as_view({'post':'add_photo','put':'delete_photo'})),
+	url(r'^/(?P<pk>\d+)/publish/?$',ActivitiesViewSet.as_view({'put':'publish'})),
+	url(r'^/(?P<activity_pk>\d+)/gallery/?$',AcitivityPhotosViewSet.as_view({'get': 'list', 'post': 'create'})),
+	url(r'^/(?P<activity_pk>\d+)/gallery/(?P<gallery_pk>\d+)/?$',AcitivityPhotosViewSet.as_view({'delete':'destroy'})),
 
-	url(r'^info/?$',ActivitiesViewSet.as_view({'get':'general_info'})),
-	url(r'^tags/?$',TagsViewSet.as_view({'get':'list'})),
-	url(r'^tag/?$',TagsViewSet.as_view({'post':'create'})),
-	url(r'^ /?$',CategoriesViewSet.as_view({'get':'list'})),
-	url(r'^category/?$',CategoriesViewSet.as_view({'post':'create'})),
-	url(r'^subcategories/?$',SubCategoriesViewSet.as_view({'get':'list'})),
-	url(r'^subcategory/?$',SubCategoriesViewSet.as_view({'post':'create'})),
+	url(r'^/info/?$',ActivitiesViewSet.as_view({'get':'general_info'})),
+	url(r'^/tags/?$',TagsViewSet.as_view({'get':'list'})),
+	url(r'^/tag/?$',TagsViewSet.as_view({'post':'create'})),
+	url(r'^/categories/?$',CategoriesViewSet.as_view({'get':'list'})),
+	url(r'^/category/?$',CategoriesViewSet.as_view({'post':'create'})),
+	url(r'^/subcategories/?$',SubCategoriesViewSet.as_view({'get':'list'})),
+	url(r'^/subcategory/?$',SubCategoriesViewSet.as_view({'post':'create'})),
 	)
---
+++ utils/mixins.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+from django.template.defaultfilters import filesizeformat
+from django.utils.translation import ugettext_lazy as _
+from guardian.shortcuts import assign_perm
+from rest_framework import serializers
+
+
+class AssignPermissionsMixin(object):
+    permissions = tuple()
+
+    def assign_permissions(self, user, instance):
+        for permission in self.permissions:
+            assign_perm(permission, user, instance)
+
+
+class FileUploadMixin(object):
+    def clean_file(self, file):
+        content_type = file.content_type.split('/')[0]
+        if content_type in settings.CONTENT_TYPES:
+            if file._size > settings.MAX_UPLOAD_PHOTO_SIZE:
+                msg = _('Mantenga el tamaño del archivo por debajo %s. Tamaño actual %s'\
+                        % (filesizeformat(settings.MAX_UPLOAD_PHOTO_SIZE), filesizeformat(file._size)))
+                raise serializers.ValidationError(msg)
+        else:
+            raise serializers.ValidationError(_('El tipo de archivo no es soportado'))
+
+        return file
