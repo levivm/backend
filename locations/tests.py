084286b5377e7bf9a0a78cbe98ecf5c9da65c317
Harvey Forero <harvey@trulii.com>
2015-05-11 17:02:42 -0500

    TRUL-186 #in-review Crear los test para los endpoints de locations

--- locations/tests.py
+++ locations/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework import status
+from locations.views import CitiesViewSet
+from utils.tests import BaseViewTest
 
-# Create your tests here.
+
+class LocationViewTest(BaseViewTest):
+    url = '/api/locations/cities'
+    view = CitiesViewSet
+
+    def test_url_should_resolve_correctly(self):
+        self.url_resolve_to_view_correctly()
+
+    def test_methods_for_anonymous(self):
+        self.method_should_be(clients=self.client, method='get', status=status.HTTP_200_OK)
+        self.authorization_should_be_require()
+
+    def test_methods_for_student(self):
+        student = self.get_student_client()
+        self.method_should_be(clients=student, method='get', status=status.HTTP_200_OK)
+        self.method_should_be(clients=student, method='post', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=student, method='put', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=student, method='delete', status=status.HTTP_403_FORBIDDEN)
+
+    def test_methods_for_organizer(self):
+        organizer = self.get_organizer_client()
+        self.method_should_be(clients=organizer, method='get', status=status.HTTP_200_OK)
+        self.method_should_be(clients=organizer, method='post', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=organizer, method='put', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=organizer, method='delete', status=status.HTTP_403_FORBIDDEN)
