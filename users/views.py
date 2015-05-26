1d86aeae888e1ebfe6585b2ff4f67ab2316019ee
Levi Velazquez <levinoelvm@gmail.com>
2015-05-26 16:31:27 -0430

    TRUL-20 #comment arreglado endpoint para cambiar el password

--- users/views.py
+++ users/views.py
@@ -225,10 +225,10 @@ class PasswordChange(APIView):
     def post(self, request, *args, **kwargs):
         _super_response = PasswordChangeView()
         _super_response.request = request._request
-        _super_response.post(request, *args, **kwargs)
-
         response, form = _set_ajax_response(_super_response)
+
         return _ajax_response(request, response, form=form)
+
 
 
 class ConfirmEmail(ConfirmEmailView):
