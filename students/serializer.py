d7e1ac9f66f358522b14b16ac59f69ca92bb6c73
Levi Velazquez <levinoelvm@gmail.com>
2015-05-26 15:30:34 -0430

    TRUL-20 #comment agregado user_type a endpoint de usuario

--- students/serializer.py
+++ students/serializer.py
@@ -1,23 +1,25 @@
 from .models import Student
 from rest_framework import serializers
 from users.serializers import UserSerializer
+from users.forms import UserCreateForm
+
 
 
 class StudentsSerializer(serializers.ModelSerializer):
-	user = UserSerializer()
+    user = UserSerializer()
     user_type = serializers.SerializerMethodField()
 
 
-	class Meta:
-		model = Student
-		fields = (
+    class Meta:
+        model = Student
+        fields = (
             'id',
-			'photo',
-			'user',
-			'gender',
-			'user',
-			'user_type',
-			)
+            'photo',
+            'user',
+            'gender',
+            'user',
+            'user_type',
+            )
 
 
     def get_user_type(self, obj):
