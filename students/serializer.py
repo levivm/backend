ebd7031277db461de15ed256ea2a110b5e240dc6
Levi Velazquez <levinoelvm@gmail.com>
2015-06-01 10:59:45 -0430

    TRUL-20 #comment validacion de foto en student serializer

--- students/serializer.py
+++ students/serializer.py
@@ -2,11 +2,13 @@ from .models import Student
 from rest_framework import serializers
 from users.serializers import UserSerializer
 from users.forms import UserCreateForm
+from utils.mixins import FileUploadMixin
 
 
 
-class StudentsSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+
+class StudentsSerializer(FileUploadMixin,serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     user_type = serializers.SerializerMethodField()
 
 
@@ -21,6 +23,8 @@ class StudentsSerializer(serializers.ModelSerializer):
             'user_type',
             )
 
+    def validate_photo(self, file):
+        return self.clean_file(file)
 
     def get_user_type(self, obj):
         return UserCreateForm.USER_TYPES[1][0]
