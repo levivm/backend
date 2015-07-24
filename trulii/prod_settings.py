40cb9d94e032f1459bbf37b37038a1ddb7ec92fd
Levi Velazquez <levi@trulii.com>
2015-07-24 16:25:50 -0500

    mend

--- trulii/prod_settings.py
+++ trulii/prod_settings.py
@@ -7,6 +7,10 @@ import dj_database_url
 # DATABASE_URL  = "postgres://trulii:trulii@localhost:5432/trulii"
 
 DATABASE_URL  = os.environ.get('RDS_DATABASE_URL')
+print("AsaDASDASD")
+print(DATABASE_URL)
+print("AsaDASDASD")
+print(DATABASE_URL)
 DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 print("AsaDASDASD")
