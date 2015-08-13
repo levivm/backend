c70e869cbccabf420bd4a59cb03a1ff38424d7b8
Harvey Forero <harvey@trulii.com>
2015-08-13 11:35:38 -0500

    TRUL-237 #comment Cambio en los count

--- payments/tests.py
+++ payments/tests.py
@@ -15,19 +15,11 @@ class PaymentLogicTest(BaseViewTest):
     payu_callback_url = '/api/payments/notification/'
     view = OrdersViewSet
 
-    def test_creditcard_declined(self):
-        client = self.get_student_client()
-        data = self.get_payment_data()
-        data['buyer']['name'] = 'DECLINED'
-        response = client.post(self.url, json.dumps(data), content_type='application/json')
-        payments = Payment.objects.all()
-        orders = Order.objects.all()
-        self.assertEqual(payments.count(), 1)
-        self.assertEqual(orders.count(), 1)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn(b'Su tarjeta ha sido rechazada', response.content)
+    def get_payments_count(self):
+        return Payment.objects.count()
 
-
+    def get_orders_count(self):
+        return Order.objects.count()
 
     def test_payu_confirmation_url_transaction_does_not_exist(self):
         client = self.get_student_client()
@@ -37,7 +29,6 @@ class PaymentLogicTest(BaseViewTest):
         }
         payu_callback_response = client.post(self.payu_callback_url,data=data)
         self.assertEqual(payu_callback_response.status_code,status.HTTP_404_NOT_FOUND)
-
 
     def test_payu_confirmation_url_transaction_expired(self):
         client = self.get_student_client()
@@ -56,7 +47,6 @@ class PaymentLogicTest(BaseViewTest):
         self.assertEqual(orders.count(),0)
         self.assertEqual(payu_callback_response.status_code,status.HTTP_200_OK)
 
-
     def test_payu_confirmation_url_transaction_declined(self):
         client = self.get_student_client()
         data = self.get_payment_data()
@@ -73,7 +63,6 @@ class PaymentLogicTest(BaseViewTest):
         orders = Order.objects.filter(id=order_id)
         self.assertEqual(orders.count(),0)
         self.assertEqual(payu_callback_response.status_code,status.HTTP_200_OK)
-
 
     def test_payu_confirmation_url_transaction_approved(self):
         client = self.get_student_client()
@@ -92,40 +81,52 @@ class PaymentLogicTest(BaseViewTest):
         self.assertEqual(order.status,ORDER_APPROVED_STATUS)
         self.assertEqual(payu_callback_response.status_code,status.HTTP_200_OK)
 
+    def test_creditcard_declined(self):
+        payments_count = self.get_payments_count()
+        orders_count = self.get_orders_count()
+        client = self.get_student_client()
+        data = self.get_payment_data()
+        data['buyer']['name'] = 'DECLINED'
+        response = client.post(self.url, json.dumps(data), content_type='application/json')
+        self.assertEqual(self.get_payments_count(), payments_count)
+        self.assertEqual(self.get_orders_count(), orders_count)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn(b'Su tarjeta ha sido rechazada', response.content)
+
     def test_creditcard_approved(self):
+        payments_count = self.get_payments_count()
+        orders_count = self.get_orders_count()
         client = self.get_student_client()
         response = client.post(self.url, json.dumps(self.get_payment_data()), content_type='application/json')
-        payments = Payment.objects.all()
-        orders = Order.objects.all()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(payments.count(), 2)
-        self.assertEqual(orders.count(), 2)
-        last_order = orders.latest('pk')
+        self.assertEqual(self.get_payments_count(), payments_count + 1)
+        self.assertEqual(self.get_orders_count(), orders_count + 1)
+        last_order = Order.objects.latest('pk')
         self.assertEqual(last_order.status, 'approved')
         self.assertEqual(last_order.assistants.count(), 1)
 
     def test_creditcard_pending(self):
+        payments_count = self.get_payments_count()
+        orders_count = self.get_orders_count()
         client = self.get_student_client()
         data = self.get_payment_data()
         data['buyer']['name'] = 'PENDING'
         response = client.post(self.url, json.dumps(data), content_type='application/json')
-        payments = Payment.objects.all()
-        orders = Order.objects.all()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(payments.count(), 2)
-        self.assertEqual(orders.count(), 2)
-        last_order = orders.latest('pk')
+        self.assertEqual(self.get_payments_count(), payments_count + 1)
+        self.assertEqual(self.get_orders_count(), orders_count + 1)
+        last_order = Order.objects.latest('pk')
         self.assertEqual(last_order.status, 'pending')
         self.assertEqual(last_order.assistants.count(), 1)
 
     def test_creditcard_error(self):
+        payments_count = self.get_payments_count()
+        orders_count = self.get_orders_count()
         client = self.get_student_client()
         data = self.get_payment_data()
         data['token'] = '1234567890'
         response = client.post(self.url, json.dumps(data), content_type='application/json')
-        payments = Payment.objects.all()
-        orders = Order.objects.all()
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(payments.count(), 1)
-        self.assertEqual(orders.count(), 1)
+        self.assertEqual(self.get_payments_count(), payments_count)
+        self.assertEqual(self.get_orders_count(), orders_count)
         self.assertIn(b'ERROR', response.content)
