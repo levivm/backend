79a2f279c514a98c8b0309c0b318048111d657f0
Harvey Forero <harvey@trulii.com>
2015-08-18 15:03:32 -0500

    Ordernes por organizador

--- orders/tests.py
+++ orders/tests.py
@@ -131,3 +131,28 @@ class ByStudentViewTest(BaseViewTest):
         self.method_should_be(clients=student, method='post', status=status.HTTP_405_METHOD_NOT_ALLOWED)
         self.method_should_be(clients=student, method='put', status=status.HTTP_403_FORBIDDEN)
         self.method_should_be(clients=student, method='delete', status=status.HTTP_403_FORBIDDEN)
+
+
+class ByOrganizerViewTest(BaseViewTest):
+    url = '/api/organizers/1/orders'
+    view = OrdersViewSet
+
+    def test_url_resolve_to_view_correctly(self):
+        self.url_resolve_to_view_correctly()
+
+    def test_authorization_should_be_require(self):
+        self.authorization_should_be_require(safe_methods=True)
+
+    def test_methods_for_organizer(self):
+        organizer = self.get_organizer_client()
+        self.method_get_should_return_data(clients=organizer)
+        self.method_should_be(clients=organizer, method='post', status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.method_should_be(clients=organizer, method='put', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=organizer, method='delete', status=status.HTTP_403_FORBIDDEN)
+
+    def test_methods_for_student(self):
+        student = self.get_student_client()
+        self.method_should_be(clients=student, method='get', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=student, method='post', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=student, method='put', status=status.HTTP_403_FORBIDDEN)
+        self.method_should_be(clients=student, method='delete', status=status.HTTP_403_FORBIDDEN)
