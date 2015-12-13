0e24fe6b29e994ab88797be1e27209d947f40ef7
Levi <levinoelvm@gmail.com>
2015-12-13 10:52:50 -0430

    TRUL-91 #comment Arreglado task para enviar correo a trulii cuando un organizador denuncia un review

--- reviews/tasks.py
+++ reviews/tasks.py
@@ -1,19 +1,21 @@
 from reviews.models import Review
 from utils.tasks import SendEmailTaskMixin
+from .serializers import ReviewSerializer
 
 
 class SendReportReviewEmailTask(SendEmailTaskMixin):
-    def run(self, review_id, template, **kwargs):
+    def run(self, review_id, **kwargs):
         review = Review.objects.get(id=review_id)
         template = "reviews/email/report_review_cc"
         kwargs['review'] = review
         return super().run(instance=review, template=template, **kwargs)
 
-    def get_emails_to(self, **kwargs):
+    def get_emails_to(self, *args, **kwargs):
         return ['contacto@trulii.com']
 
     def get_context_data(self, data):
         review = data.get('review')
+        review_data = ReviewSerializer(instance=review).data
         return {
-            'review': review
+            'review': review_data
         }
