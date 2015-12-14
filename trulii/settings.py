f4f596383a97e84a20a7552e017d0af31b2863d5
Harvey Forero <harvey@trulii.com>
2015-12-14 11:18:58 -0500

    Cambiar la url por defecto de admin e instalar honeypot

--- trulii/settings.py
+++ trulii/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
+    'admin_honeypot',
     'landing',
     'activities',
     'locations',    
