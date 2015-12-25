e190a2b90414c1df008764b64397750da3a5170e
Levi <levinoelvm@gmail.com>
2015-12-25 19:00:10 -0430

    #Hotfix Configurando redis CONSTANTS para usarlo con docker

--- trulii/local_settings.py
+++ trulii/local_settings.py
@@ -15,8 +15,8 @@ REDIS_HOST_ADDR = os.environ.get('REDIS_1_PORT_6379_TCP_ADDR','localhost')
 REDIS_PORT = os.environ.get('REDIS_1_PORT_6379_TCP_PORT','6379')
 
 # BROKER_URL = 'redis://localhost:6379/0'
-BROKER_URL = 'redis://{host}:{port}/0'.format(REDIS_HOST_ADDR, REDIS_PORT)
-CELERY_RESULT_BACKEND = 'redis://{host}/0'.format(REDIS_HOST_ADDR)
+BROKER_URL = 'redis://{host}:{port}/0'.format(host=REDIS_HOST_ADDR, port=REDIS_PORT)
+CELERY_RESULT_BACKEND = 'redis://{host}/0'.format(host=REDIS_HOST_ADDR)
 CELERY_ALWAYS_EAGER = False
 
 ################ / REDIS CONFIG #################
