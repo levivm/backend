5f84136399948b7060fbb97abf6434c4cb8791f2
Levi Velazquez <levi@trulii.com>
2016-03-09 11:27:09 -0500

    TRUL-86 #comment Agregado template de reembolso al tasks de reembolso para estudiante y organizador

--- landing/tasks.py
+++ landing/tasks.py
@@ -6,6 +6,6 @@ class SendContactFormEmailTask(SendEmailTaskMixin):
     def run(self, data, *args, **kwargs):
         self.template_name = "landing/email/contact_us.html"
         self.emails = ['levi@trulii.com']
-        self.subject = 'Subject contact form'
+        self.subject = 'Nuevo mensaje de Contact'
         self.global_context = data
         return super(SendContactFormEmailTask, self).run(*args, **kwargs)
