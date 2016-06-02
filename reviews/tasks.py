b8575d62e9d3485f2a167ab3d43e909b588bf922
Harvey Forero <harvey@trulii.com>
2016-06-02 10:10:25 -0500

    TRUL-373 #in-review tarea periodica para enviar recordatorio de review

--- reviews/tasks.py
+++ reviews/tasks.py
@@ -1,3 +1,9 @@
+from collections import defaultdict
+
+from dateutil.relativedelta import relativedelta, MO
+from django.utils.timezone import now
+
+from activities.models import Calendar
 from reviews.models import Review
 from utils.tasks import SendEmailTaskMixin
 
@@ -69,3 +75,31 @@ class SendReplyToStudentEmailTask(SendEmailTaskMixin):
             'reply': self.review.reply,
             'url': self.review.get_student_frontend_url(),
         }
+
+
+class SendReminderReviewEmailTask(SendEmailTaskMixin):
+    def run(self, *args, **kwargs):
+        self.data = self.get_data()
+        if self.data:
+            self.emails = self.get_emails()
+            self.template_name = 'reviews/email/send_reminder_review.html'
+            self.subject = 'Recuerda dejarnos tu review de la actividad'
+            self.global_context = {}
+            return super(SendReminderReviewEmailTask, self).run(*args, **kwargs)
+
+    def get_data(self):
+        last_monday = now() + relativedelta(weekday=MO(-1), hour=0, minute=0)
+        last_week_monday = last_monday - relativedelta(weeks=1)
+        calendars = Calendar.objects.filter(initial_date__range=(last_week_monday, last_monday))
+        data = defaultdict(list)
+        for calendar in calendars:
+            students = [o.student for o in calendar.orders.available()
+                        if not Review.objects.filter(activity=calendar.activity, author=o.student)]
+
+            for student in students:
+                data[student.user.email].append(calendar.activity)
+
+        return data
+
+    def get_emails(self):
+        return list(self.data.keys())
--- trulii/celery.py
+++ trulii/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       os.environ.get('DJANGO_SETTINGS_FILE', 'trulii.settings.local'))
@@ -13,3 +14,12 @@ app = Celery('trulii')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.update(CELERY_ACCEPT_CONTENT = ['pickle', 'json'])
+app.conf.update(
+    CELERYBEAT_SCHEDULE = {
+        # Executes every Wednesday at 12:00 A.M
+        'add-every-wednesday-noon': {
+            'task': 'reviews.tasks.SendReminderReviewEmailTask',
+            'schedule': crontab(hour=12, minute=00, day_of_week=3),
+        },
+    }
+)
