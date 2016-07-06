0057c3b21a67e507f06810e5463699111f4b5c08
Harvey Forero <harvey@trulii.com>
2016-07-06 17:22:39 -0500

    #hotfix Enviar cupón de referrer por email

--- referrals/management/commands/load_coupons.py
+++ referrals/management/commands/load_coupons.py
@@ -7,12 +7,15 @@ class Command(BaseCommand):
     help = "Create initial cuopons"
 
     def get_coupons_data(self):
-    	return [{
-    		'name':'referred',
-    		'type':'referral',
-    		'amount':20000
-    	},]
+        return [{
+            'name': 'referred',
+            'type': 'referral',
+            'amount': 20000
+        }, {
+            'name': 'referrer',
+            'type': 'referral',
+            'amount': 20000
+        }]
 
     def handle(self, *args, **options):
-    	[CouponType.objects.get_or_create(**data) for data in self.get_coupons_data()]
-    	
+        [CouponType.objects.get_or_create(**data) for data in self.get_coupons_data()]
