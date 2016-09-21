e7310bfa82dec98e9a69a7cc2c6c9a15d9ebeabb
Levi Velazquez <levi@trulii.com>
2016-09-21 17:51:08 -0500

    #hotfix Arreglado nombre al enviar correo

--- landing/tasks.py
+++ landing/tasks.py
@@ -5,7 +5,7 @@ class SendContactFormEmailTask(SendEmailTaskMixin):
 
     def run(self, data, *args, **kwargs):
         self.template_name = "landing/email/contact_us.html"
-        self.emails = ['contact@trulii.com']
+        self.emails = ['alo@trulii.com']
         self.subject = 'Nuevo mensaje de contacto'
         self.global_context = data
         return super(SendContactFormEmailTask, self).run(*args, **kwargs)
