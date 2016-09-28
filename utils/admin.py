ce78f368d6be157c581c83c34250cbd147b213d8
Harvey Forero <harvey@trulii.com>
2016-09-28 11:22:59 -0500

    #comment Mejora la vista de emails en el admin

--- utils/admin.py
+++ utils/admin.py
@@ -8,3 +8,5 @@ from utils.models import EmailTaskRecord
 class EmailTaskRecordAdmin(OperativeModelAdminMixin, admin.ModelAdmin):
     operative_readonly_fields = {'to', 'template_name', 'subject', 'data', 'status',
                                  'reject_reason', 'task_id', 'smtp_id'}
+    list_display = ('to', 'subject', 'status', 'template_name', 'task_id')
+    list_filter = ['status', 'date']
